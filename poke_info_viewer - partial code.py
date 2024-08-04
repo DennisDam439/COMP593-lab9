63cc91275696645b264b50a73df7760251948f90
DennisDam439 <dend@flemingcollege.ca>
2024-08-04 12:53:15 -0400

    Update poke_info_viewer - partial code.py

--- poke_info_viewer - partial code.py
+++ poke_info_viewer - partial code.py
@@ -43,7 +43,7 @@ def handle_btn_get_info():
     poke_info = get_pokemon_info(poke_name)
     if poke_info:
         lbl_height_val['text'] = str(poke_info['height']) + ' dm'
-        #DO FOR Weight
+        lbl_weight_val['text'] = str(poke_info['weight']) + ' hg'
         types_list = [t['type']['name'].capitalize() for t in poke_info['types']]
        
         #-----stats
@@ -51,10 +51,10 @@ def handle_btn_get_info():
         bar_hp['value'] = poke_info['stats'][0]['base_stat']
         bar_attack['value'] = poke_info['stats'][1]['base_stat']
         ##Finish the rest!!
-        #bar_defense
-        #bar_special_attack
-        #bar_special_defense
-        #bar_speed
+        bar_defense['value'] = poke_info['stats'][2]['base_stat']
+        bar_special_attack['value'] = poke_info['stats'][3]['base_stat']
+        bar_special_defense['value'] = poke_info['stats'][4]['base_stat']
+        bar_speed['value'] = poke_info['stats'][5]['base_stat']
     else:
         error_message ="test "## finish this per requirements
         messagebox.showinfo(title ='Error', message =  error_message, icon='error')
@@ -74,7 +74,7 @@ lbl_weight.grid(row=1, column=0, padx=(10,5), pady=10)
 lbl_weight_val = ttk.Label(frm_info, width =20)
 lbl_weight_val.grid(row=1, column=1, padx=(10,5), pady=10)
 
-## Do the same for Weight
+## Do the same for type
 lbl_type = ttk.Label(frm_info, text="type: ")
 lbl_type.grid(row=2, column=0, padx=(10,5), pady=10)
 lbl_type_val = ttk.Label(frm_info, width =20)
@@ -84,7 +84,7 @@ lbl_type_val.grid(row=2, column=1, padx=(10,5), pady=10)
 STATE_MAX_VALUE = 255.0
 PRG_BAR_LENGHT = 200
 
-label_hp = ttk.Lable(Frm_stats, text = "HP:")
+label_hp = ttk.Lable(frm_stats, text = "HP:")
 lbl_hp.grid(row=0, column=0, padx=(10,5), pady=(10,5), sticky=E)
 bar_hp = ttk.Progressbar(frm_stats, length=PRG_BAR_LENGTH, maximum=STAT_MAX_VALUE)
 bar_hp.grid(row=0, column=1, padx=(0,10), pady=(10,5))
@@ -95,4 +95,33 @@ bar_attack = ttk.Progressbar(frm_stats, length=PRG_BAR_LENGTH, maximum=STAT_MAX_
 bar_attack.grid(row=1, column=1, padx=(0,10), pady=5)
 ## Do the same for Defense, Special attack, Special Defense
 
+lbl_defense = ttk.Label(frm_stats, text="Defense:")
+lbl_defense.grid(row=2, column=0, padx=(10,5), pady=5, sticky=E)
+bar_defense = ttk.Progressbar(frm_stats, length=PRG_BAR_LENGTH, maximum=STAT_MAX_VALUE)
+bar_defense.grid(row=2, column=1, padx=(0,10), pady=5)
+
+lbl_special_attack = ttk.Label(frm_stats, text="Special Attack:")
+lbl_special_attack.grid(row=2, column=0, padx=(10,5), pady=5, sticky=E)
+bar_special_attack = ttk.Progressbar(frm_stats, length=PRG_BAR_LENGTH, maximum=STAT_MAX_VALUE)
+bar_special_attack.grid(row=2, column=1, padx=(0,10), pady=5)
+
+lbl_special_defense = ttk.Label(frm_stats, text="Special Defense:")
+lbl_special_defense.grid(row=2, column=0, padx=(10,5), pady=5, sticky=E)
+bar_special_defense = ttk.Progressbar(frm_stats, length=PRG_BAR_LENGTH, maximum=STAT_MAX_VALUE)
+bar_special_defense.grid(row=2, column=1, padx=(0,10), pady=5)
+
+
+
+
+
+
+
+
+
+
+
+
+
 root.mainloop()
+
+
